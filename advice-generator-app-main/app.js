536283ecba6fe2cda5c88a002511f64b0defd619
Ifycode <marychord@gmail.com>
2022-03-30 01:00:49 +0100

    Add and animate loader

--- advice-generator-app-main/app.js
+++ advice-generator-app-main/app.js
@@ -12,25 +12,41 @@ const adviceText = document.getElementById('advice__text');
     fetch('https://api.adviceslip.com/advice')
     .then(response => response.json())
     .then(data => {
+        handleLoader('done', data);
         localStorage.setItem('data', JSON.stringify(data));
         displayedData(data);
         console.log(data);
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log(err)); // You can add UI for showing error message too if an error occrs (if you like)
+ }
+
+ function handleLoader (status, api_data) {
+    const ls_data = JSON.parse(localStorage.getItem('data'));
+     const loader = document.querySelector('#loader');
+     loader.classList.remove('fadeout');
+    if (status === 'loading') {
+        loader.innerHTML = 'Loading...';
+        loader.classList.add('block');
+    } else {
+        loader.innerHTML = `API returned advice ${api_data.slip.id} ${api_data.slip.id === ls_data.slip.id ? 'again': ''}`;
+        loader.classList.add('fadeout');
+    }
  }
 
  function getFromLocalStorage() {
-    const data = JSON.parse(localStorage.getItem('data'));
-    if (data) return displayedData(data);
+    const ls_data = JSON.parse(localStorage.getItem('data'));
+    if (ls_data) return displayedData(ls_data);
         else return displayedData({"slip": { "id": 205, "advice": "Try to not compliment people on things they don't control."}});
  }
 
-window.onload = function() {
+window.addEventListener('load', function() {
+    handleLoader('loading', null);
     getFromLocalStorage();
     fetchAdvice();
-}
+});
 
  click.addEventListener('click', function() {
+    handleLoader('loading', null);
     getFromLocalStorage();
     fetchAdvice();
  });
